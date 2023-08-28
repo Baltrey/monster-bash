6bfb8ee403539b98d1cd06a2742becf08aa78cab
Baltazar Biverstal <baltazar.biverstal@elev.ga.ntig.se>
2023-08-28 13:03:26 +0200

    first commit

---
+++ monster-bash/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
+
+Player player = new();
+Enemy enemy = new();
+Console.WriteLine("Hej vad är ditt namn?");
+player.Name = Console.ReadLine();
+while (true)
+{
+    while (enemy.HP > 0)
+    {
+        Console.WriteLine("tryck enter för att slå");
+        Console.ReadLine();
+        enemy.HP -= player.attack;
+        Console.WriteLine("enemy har " + enemy.HP + " HP kvar");
+    }
+    bool active = true;
+    while (active)
+    {
+        Console.WriteLine("vill du köra igen? Y/N");
+        string answer = Console.ReadLine().ToUpper();
+
+        if (answer == "Y" || answer == "YES")
+        {
+            active = false;
+        }
+        if (answer == "N" || answer == "NO")
+        {
+            return;
+        }
+    }
+}
---
+++ monster-bash/class.cs
@@ -0,0 +1,10 @@
+public class Player
+{
+    public string Name;
+    public int attack = 10;
+}
+public class Enemy
+{
+    public bool isdead = false;
+    public int HP = 100;
+}
